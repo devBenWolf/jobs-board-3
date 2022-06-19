599f7ca0491cf6b2882dfcea4cc8314558179dc8
BenJohannson <57221492+BenJohannson@users.noreply.github.com>
2022-06-19 19:03:14 -0400

    js:
    removed currentData state from component

--- src/containers/MainSearchContainer.js
+++ src/containers/MainSearchContainer.js
@@ -5,7 +5,7 @@ import { DataContext } from "../contexts/DataContext"
 import {ThemeContext} from "../contexts/ThemeContext"
 
 const MainSearchContainer = () => {
-    const {currentData, mainInputData, jobs} = useContext(DataContext)
+    const {mainInputData, jobs} = useContext(DataContext)
     const {themeBoolean} = useContext(ThemeContext)
 
 
