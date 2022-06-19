2623ffc85174600016bc60481a1d9cae823501d1
BenJohannson <57221492+BenJohannson@users.noreply.github.com>
2022-06-19 18:58:51 -0400

    js:
    added code for local storage, exported parsed local storage variable "localJobs"

--- src/contexts/DataContext.js
+++ src/contexts/DataContext.js
@@ -18,13 +18,18 @@ const DataContextProvider = ({children}) => {
         const data = await getDocs(jobsCollectionRef)
         setJobs(data.docs.map((item) => ({...item.data(), id: item.id})))
         setUnfilteredJobs(data.docs.map((item) => ({...item.data(), id: item.id})))
+        localStorage.setItem("localJobs", JSON.stringify(data.docs.map((item) => ({...item.data(), id: item.id}))))
       }
       getJobs()
+      
     }, [])
+    
+    const localObject = localStorage.getItem("localJobs")
+    const localJobs = JSON.parse(localObject)
 
     return ( 
         <DataContext.Provider 
-            value={{currentData, setCurrentData, mainInputData, setMainInputData,
+            value={{currentData, setCurrentData, mainInputData, setMainInputData, localJobs,
                 locationInputData, setLocationInputData, jobs, setJobs, unfilteredJobs
             }}
         >
